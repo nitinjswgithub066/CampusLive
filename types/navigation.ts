326803732fcea8c369359e0a77937824a4c65bc8
Siddhartha Gautam <sid.2005.19@gmail.com>
2026-02-16 22:46:54 +0530

    Updated the file structure.

---
+++ types/navigation.ts
@@ -0,0 +1,3 @@
+/**
+ * Naviagation Param List
+ */
---
+++ utils/passwordStrength.ts
@@ -0,0 +1,3 @@
+/**
+ * This file will contain the logic for checking the strength of a password. It will be used in the registration and change password screens to provide feedback to the user about the strength of their password.
+ */
